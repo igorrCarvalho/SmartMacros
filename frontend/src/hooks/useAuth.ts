cbd89fe8a6bb78e6371486f1a3471274c9e8d064
igorrCarvalho <igorsilvabhz6@gmail.com>
2025-11-03 04:47:40 -0300

    feat<frontend>: useAuth hook

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,5 @@
+export function useAuth() {
+  const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  const isAuthenticated = !!token;
+  return { isAuthenticated, token };
+}
