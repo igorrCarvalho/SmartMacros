c91cb9c1b22e4f1c6de76b0738056626ba96ff3f
igorrCarvalho <igorsilvabhz6@gmail.com>
2025-11-04 23:10:00 -0300

    feat<frontend>: types

--- frontend/src/types/index.tsx
+++ frontend/src/types/index.tsx
@@ -1,3 +1,11 @@
+import type {
+  QueryKey,
+  UseQueryOptions,
+  UseQueryResult,
+  UseMutationOptions,
+  UseMutationResult,
+} from "@tanstack/react-query";
+
 export type ShadTooltipProps = {
   content: string;
   children: React.ReactNode;
@@ -11,5 +19,39 @@ export type UserAuthenticationProps = {
 export type AuthenticationStore = {
   isAuth: boolean;
   email: string;
+  password: string;
+  setPassword: (password: string) => void;
   setEmail: (email: string) => void;
 }
+
+export type AuthenticationFormProps = {
+  submitMethod: () => void;
+  switchButtonMethod: () => void;
+  title: string;
+  description: string;
+  submitLabel: string;
+  switchButtonLabel: string;
+}
+
+export type SignUpBody = {
+  email: string;
+  password: string;
+}
+
+export type SignUpResponse = {
+  id: string;
+  email: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export type useQueryFunctionType<Params, Data> = (
+  params: Params,
+  options?: Omit<UseQueryOptions<Data, Error>, "queryKey" | "queryFn"> & {
+    queryKey?: QueryKey;
+  }
+) => UseQueryResult<Data, Error>;
+
+export type useMutationFunctionType<Vars, Data> = (
+  options?: Omit<UseMutationOptions<Data, Error, Vars>, "mutationFn">
+) => UseMutationResult<Data, Error, Vars>;
