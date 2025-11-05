ebadebcb301a50417ff02ace240271f41e6a70e1
igorrCarvalho <igorsilvabhz6@gmail.com>
2025-11-04 22:57:51 -0300

    feat<backend>: add cors middleware to app router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from app.core import session_filters
 from app.api.routers import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="SmartMacros API", version="0.1.0")
 app.include_router(api_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:9999",
+        "http://127.0.0.1:9999",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*", "Authorization"],
+)
 
 @app.get("/health")
 def health():
