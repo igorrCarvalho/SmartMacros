6099baeac5d8c622a9ce586776a8efc4e3c130c6
igorrCarvalho <igorsilvabhz6@gmail.com>
2025-11-06 03:49:23 -0300

    feat<frontend>: update AuthenticationStore to work with refresh token

--- frontend/src/stores/useAuthenticationStore.ts
+++ frontend/src/stores/useAuthenticationStore.ts
@@ -3,7 +3,30 @@ import type { AuthenticationStore } from "../types";
 
 export const useAuthenticationStore = create<AuthenticationStore>((set, get) => ({
     isAuth: false,
+    token: "",
     email: "",
+    password: "",
+    isHydrated: false,
+    logout: () => {
+        set({
+            isAuth: false,
+            token: null,
+            email: "",
+            password: "",
+        });
+    },
+    setIsHydrated: (isHydrated) => {
+        set({ isHydrated });
+    },
+    setIsAuth: (isAuth) => {
+        set({ isAuth });
+    },
+    setToken: (token) => {
+        set({ token, isAuth: true });
+    },
+    setPassword: (password) => {
+        set({ password });
+    },
     setEmail: (email) => {
         set({ email });
     },
