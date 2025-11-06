a2f71b88b2f28eff3d768f70509f078b45e2040c
igorrCarvalho <igorsilvabhz6@gmail.com>
2025-11-06 03:52:41 -0300

    feat<frontend>: use query and auth providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
+import { QueryClientProvider } from '@tanstack/react-query';
 import { router } from "./router";
 import './App.css'
+import { queryClient } from './api/queryClient';
+import AuthProvider from './api/providers/AuthProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient }>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
