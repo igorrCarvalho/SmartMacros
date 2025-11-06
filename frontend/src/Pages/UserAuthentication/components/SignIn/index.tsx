c2448ffd30ea608341246a100d735f066b4a66d4
igorrCarvalho <igorsilvabhz6@gmail.com>
2025-11-06 03:53:49 -0300

    feat<frontend>: signIn and signOut pages

--- frontend/src/Pages/UserAuthentication/components/SignIn/index.tsx
+++ frontend/src/Pages/UserAuthentication/components/SignIn/index.tsx
@@ -1,15 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import AuthenticationForm from "../AuthenticationForm";
+import { usePostLogin } from "../../../../api/queries/authentication/usePostLogin";
+import { useAuthenticationStore } from "../../../../stores/useAuthenticationStore";
 
 export default function SignIn({}) {
     const nav = useNavigate();
+    const { email, password } = useAuthenticationStore((state) => state);
+    const { mutate: loginMutate, isPending: isLoginPending } = usePostLogin();
+
+    function handleUserSignIn(e) {
+        e.preventDefault();
+        loginMutate({ email, password }, {
+            onSuccess: (loginData) => {
+                useAuthenticationStore.getState().setToken(loginData.access_token);
+                nav("/dashboard");
+            }
+        });
+    }
     return (
         <AuthenticationForm
             title="Sign In to SmartMacros"
             description="Enter your email below to login to your account"
-            submitMethod={() => {}}
+            submitMethod={handleUserSignIn}
             submitLabel={"Login"}
-            switchButtonMethod={() => nav("/signup")}
+            switchButtonMethod={(e) => {
+                e.preventDefault();
+                nav("/signup");
+            }}
             switchButtonLabel={"Sign Up"}
         />
     );
--- frontend/src/Pages/UserAuthentication/components/SignUp/index.tsx
+++ frontend/src/Pages/UserAuthentication/components/SignUp/index.tsx
@@ -1,15 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import AuthenticationForm from "../AuthenticationForm";
+import { usePostUser } from "../../../../api/queries/authentication/usePostUser";
+import { useAuthenticationStore } from "../../../../stores/useAuthenticationStore";
+import { usePostLogin } from "../../../../api/queries/authentication/usePostLogin";
 
 export default function SignUp({}) {
     const nav = useNavigate();
+    const { email, password } = useAuthenticationStore((state) => state);
+    const { mutate, isPending } = usePostUser();
+    const { mutate: loginMutate, isPending: isLoginPending } = usePostLogin();
+
+    function handleUserSubmit(e) {
+        e.preventDefault();
+        mutate({ email, password }, {
+            onSuccess: () => {
+                loginMutate({ email, password }, {
+                    onSuccess: (loginData) => {
+                        useAuthenticationStore.getState().setToken(loginData.access_token);
+                        nav("/dashboard");
+                    }
+                });
+            },
+        });
+    }
     return (
         <AuthenticationForm
             title="Register to SmartMacros"
             description="Enter your email below to create your account"
-            submitMethod={() => {}}
+            submitMethod={handleUserSubmit}
             submitLabel={"Register"}
-            switchButtonMethod={() => nav("/signin")}
+            switchButtonMethod={(e) => {
+                e.preventDefault();
+                nav("/signin");
+            }}
             switchButtonLabel={"Sign In"}
         />
     );
